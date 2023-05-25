ba3123527505c823be56769c4fcf6405351aef3f
Gulnihal Coskun <glnhcsk.22@gmail.com>
2023-05-25 23:20:15 +0300

    updated

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -3,15 +3,17 @@
 *********************Surname:COSKUN*********************
 *********************Course Name:SWE202 SOFTWARE VERIFICATION AND VALIDATION
 *********************Assignment1*************************
- ********************GitHub Repository address: https://github.com/gulnihalcoskunn/Assignment.git */
+ ********************GitHub Repository address: https://github.com/gulnihalcoskunn/Assignment1.git */
 
 
 package org.example;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorTest {
     @ParameterizedTest
@@ -24,5 +26,15 @@ class CalculatorTest {
     })
     public void testDivision(double dividend, double divisor, double expected) {
         assertEquals(expected, Calculator.divide(dividend, divisor));
+    }
+
+    @Test
+    void testDivision6() {
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> Calculator.divide(12.5, 0),
+                "IllegalArgumentException expected."
+        );
     }
 }
